9e81b18150b86dffb304185b551819c8e3b9e29e
martinuiga <uigakarlmartin@gmail.com>
2019-03-25 15:36:54 +0200

    Data binding done

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,13 +8,19 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('Test recipe', 'This is a test.', 'https://images.media-allrecipes.com/images/56589.png')
+    new Recipe('Test recipe', 'This is a test', 'https://images.media-allrecipes.com/images/56589.png'),
+    new Recipe('New test recipe', 'This is a test', 'https://images.media-allrecipes.com/images/56589.png')
   ];
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
